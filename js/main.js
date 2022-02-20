4cec6cbeece1cfce0b4dfbd9595c9a286984c6d5
Дарья <yu.dar@yandex.ru>
2022-02-20 17:26:03 +0200

    Добавление тернарнорного оператора

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
-function getRundomNumber(min, max) {
+// Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random;
+const getRundomNumber = (min, max) => {
   if (min < 0 || max <= min) {
-    return;
+    return undefined;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 getRundomNumber(1,4);
 
 // Функция для проверки максимальной длины строки.
-function checkMaxLength(text, maxNumberOfSymbols) {
-  if (text.length >= maxNumberOfSymbols) {
-    return false;
-  }
-  return true;
-}
+const checkMaxLength = (text, maxNumberOfSymbols) => text.length <= maxNumberOfSymbols;
 
 checkMaxLength('привет', 4);
