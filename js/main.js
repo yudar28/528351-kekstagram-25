de5bb5d2b356b3582f730051e49f31de6889efb2
Дарья <yu.dar@yandex.ru>
2022-03-03 00:44:43 +0200

    Исправление ошибок

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно.
-// Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random;
-const getRundomNumber = (firstNumber, secondNumber) => {
-  let min = firstNumber;
-  let max = secondNumber;
-  if (firstNumber > secondNumber) {
-    min = secondNumber;
-    max = firstNumber;
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-// Функция для проверки максимальной длины строки.
-const checkMaxLength = (text, maxNumberOfSymbols) => text.length <= maxNumberOfSymbols;
-
-checkMaxLength('привет', 4);
-
 const NAMES = [
   'Иван',
   'Артем',
@@ -36,52 +19,72 @@ const MESSAGE = [
   'Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.',
 ];
 
+const MAX_NUMBER_COMMENTS = 4;
+
+const MAX_NUMBER_ARRAY_COMMENTS = 100;
+
+const MIN_LIKES = 15;
+
+const MAX_LIKES = 200;
+
+
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
+// Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random;
+const getRandomNumber = (firstNumber, secondNumber) => {
+  let min = firstNumber;
+  let max = secondNumber;
+  if (firstNumber > secondNumber) {
+    min = secondNumber;
+    max = firstNumber;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Функция для проверки максимальной длины строки.
+const checkMaxLength = (text, maxNumberOfSymbols) => text.length <= maxNumberOfSymbols;
+
+checkMaxLength('привет', 4);
+
 //Функция, которая возвращает массив перемешенных чисел от 1 до length
 //Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/from
-const getRundomArrayNumber = (length) => {
-  const arr = Array.from({ length: length }, (_v, k) => ++k);
-  return arr.sort(() => Math.random()-0.5);
+const getRandomArray = (length) => {
+  const arr = Array.from({ length }, (_v, k) => k + 1);
+  return arr.sort(() => Math.random() - 0.5);
 };
 
 // Функция, возрвращающая случайный элемент массива
-const getRundomArrayElement = (elements) => elements[getRundomNumber(0, elements.length -1)];
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length -1)];
 
 const comments = [];
 
 const createComment = (id) => ({
   id: id,
-  avatar: `img/avatar-${getRundomNumber(1,6)}.svg`,
-  message: getRundomArrayElement(MESSAGE),
-  name: getRundomArrayElement(NAMES),
+  avatar: `img/avatar-${getRandomNumber(1,6)}.svg`,
+  message: getRandomArrayElement(MESSAGE),
+  name: getRandomArrayElement(NAMES),
 });
 
-getRundomArrayNumber(100).forEach((value) => {
+getRandomArray(MAX_NUMBER_ARRAY_COMMENTS).forEach((value) => {
   comments.push(createComment(value));
 });
 
-function getRundomArray (arr) {
+const getRandomArrayOfComments = (arr) => {
   const newArray = [];
-  for (let i=1; i<=getRundomNumber(1,4); i++) {
-    newArray.push(getRundomArrayElement(arr));
+  for (let i = 1; i <= getRandomNumber(1,MAX_NUMBER_COMMENTS); i++) {
+    newArray.push(getRandomArrayElement(arr));
   }
   return newArray;
-}
+};
 
 const createPhotoDescription = (number) => ({
   id: number,
   url: `photos/${number}.jpg`,
   description: 'Описание фотографии',
-  likes: getRundomNumber(15, 200),
-  comments: getRundomArray(comments),
+  likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+  comments: getRandomArrayOfComments(comments),
 });
 
-const getPhotosData = (numberPhotos) => {
-  const similarDescriptions = [];
-  getRundomArrayNumber(numberPhotos).forEach((value) => {
-    similarDescriptions.push(createPhotoDescription(value));
-  });
-  return similarDescriptions;
-};
+const getPhotosData = (count) => getRandomArray(count).map((value) => createPhotoDescription(value));
 
 getPhotosData(25);
 
--- js/test.js
+++ js/test.js
@@ -1,8 +1,7 @@
 
 const getRundomArrayNumber = (length) => {
-  // eslint-disable-next-line no-shadow
-  const arr = Array.from({ length: length }, (_v, k) => ++k);
-  return arr.sort(() => Math.random()-0.5);
+  const arr = Array.from({ length }, (_v, k) => k + 1);
+  return arr.sort(() => Math.random() - 0.5);
 };
 
 getRundomArrayNumber(8);
