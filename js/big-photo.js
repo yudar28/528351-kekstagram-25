8b7e1796b655ea519b343b9ad7d1357fa398022e
Дарья <yu.dar@yandex.ru>
2022-03-11 22:45:58 +0200

    добавление больше функций в big-photo.js

--- js/big-photo.js
+++ js/big-photo.js
@@ -1,6 +1,6 @@
-import { useresPhotoList, similarPhotoDatas } from './users.js';
+import { usersPhotoList, similarPhotoData } from './users.js';
 
-const useresPhotoItems = Array.from(useresPhotoList.getElementsByClassName('picture'));
+const useresPhotoItems = Array.from(usersPhotoList.querySelectorAll('.picture'));
 const body = document.querySelector('body');
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureImg = bigPicture.querySelector('.big-picture__img').querySelector('img');
@@ -13,40 +13,50 @@ const buttonCloseBigPicture = bigPicture.querySelector('.big-picture__cancel');
 const commentCouner = bigPicture.querySelector('.social__comment-count');
 const commentLoader= bigPicture.querySelector('.comments-loader');
 
+const createComment = (item) => {
+  const newComment = document.createElement('li');
+  newComment.classList.add('social__comment');
+
+  const newCommentImg = document.createElement('img');
+  newCommentImg.classList.add('social__picture');
+  newCommentImg.src = item.avatar;
+  newCommentImg.alt = item.name;
+  newCommentImg.width = '35';
+  newCommentImg.height = '35';
+  newComment.append(newCommentImg);
+
+  const newCommentText = document.createElement('p');
+  newCommentText.textContent = item.message;
+  newComment.append(newCommentText);
+
+  commentsList.append(newComment);
+};
+
+const getBigPicture = (k) => {
+  bigPicture.classList.remove('hidden');
+  body.classList.add('modal-open');
+
+  commentCouner.classList.add('hidden'); //Временно
+  commentLoader.classList.add('hidden'); //Временно
+
+  const PhotoData = similarPhotoData[k];
+
+  bigPictureImg.src = PhotoData.url;
+  bigPictureLikes.textContent = PhotoData.likes;
+  bigPictureCommentsCounter.textContent = PhotoData.comments.length;
+  bigPictureCaption.textContent = PhotoData.description;
+
+  commentsList.innerHTML = '';
+
+  similarPhotoData[k].comments.forEach((comment) => {
+    createComment(comment);
+  });
+};
+
 
 useresPhotoItems.forEach((item, i) => {
   item.addEventListener('click', () => {
-    bigPicture.classList.remove('hidden');
-    body.classList.add('modal-open');
-
-    commentCouner.classList.add('hidden'); //Временно
-    commentLoader.classList.add('hidden'); //Временно
-
-    bigPictureImg.src = similarPhotoDatas[i].url;
-    bigPictureLikes.textContent = similarPhotoDatas[i].likes;
-    bigPictureCommentsCounter.textContent = similarPhotoDatas[i].comments.length;
-    bigPictureCaption.textContent = similarPhotoDatas[i].description;
-
-    commentsList.innerHTML = '';
-
-    similarPhotoDatas[i].comments.forEach((comment) => {
-      const newComment = document.createElement('li');
-      newComment.classList.add('social__comment');
-
-      const newCommentImg = document.createElement('img');
-      newCommentImg.classList.add('social__picture');
-      newCommentImg.src = comment.avatar;
-      newCommentImg.alt = comment.name;
-      newCommentImg.width = '35';
-      newCommentImg.height = '35';
-      newComment.append(newCommentImg);
-
-      const newCommentText = document.createElement('p');
-      newCommentText.textContent = comment.message;
-      newComment.append(newCommentText);
-
-      commentsList.append(newComment);
-    });
+    getBigPicture(i);
   });
 });
 
--- js/users.js
+++ js/users.js
@@ -1,13 +1,13 @@
 import { getPhotosData } from './data-photos.js';
 
 const similarUserPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const useresPhotoList = document.querySelector('.pictures');
+const usersPhotoList = document.querySelector('.pictures');
 
-const numberOfPhotosDatas = 25;
-const similarPhotoDatas = getPhotosData(numberOfPhotosDatas);
+const PHOTOS_COUNT = 25;
+const similarPhotoData = getPhotosData(PHOTOS_COUNT);
 const similarListFragment = document.createDocumentFragment();
 
-similarPhotoDatas.forEach(({ url, likes, comments }) => {
+similarPhotoData.forEach(({ url, likes, comments }) => {
   const userPhotoElement = similarUserPhotoTemplate.cloneNode(true);
   userPhotoElement.querySelector('.picture__img').src = url;
   userPhotoElement.querySelector('.picture__likes').textContent = likes;
@@ -16,6 +16,6 @@ similarPhotoDatas.forEach(({ url, likes, comments }) => {
   similarListFragment.append(userPhotoElement);
 });
 
-useresPhotoList.append(similarListFragment);
+usersPhotoList.append(similarListFragment);
 
-export { useresPhotoList, similarPhotoDatas };
+export { usersPhotoList, similarPhotoData };
